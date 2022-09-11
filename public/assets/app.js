9613c506010be7d41db1a214c65d3857dfc749d4
Amaranth01 <vanessa.nectoux@gmail.com>
2022-09-11 08:23:31 +0200

    Improved animation

--- public/assets/app.js
+++ public/assets/app.js
@@ -3,11 +3,15 @@ let logo = document.querySelectorAll('img');
 function zoom() {
     for(let i = 0; i < logo.length; i++) {
         logo[i].addEventListener("mouseover", function () {
-            logo[i].style.transform = "scale(2)"
-        })
+            logo[i].style.transform = "scale(2.5)";
+            logo[i-1].style.transform = "scale(1.25)";
+            logo[i+1].style.transform = "scale(1.25)";
+        });
         logo[i].addEventListener("mouseleave", function () {
-            logo[i].style.transform = "scale(1)"
-        })
+            logo[i].style.transform = "scale(1)";
+            logo[i-1].style.transform = "scale(1)";
+            logo[i+1].style.transform = "scale(1)";
+        });
     }
 }
 
