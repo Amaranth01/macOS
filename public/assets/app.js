dc0d57c769560586266ecadd18147c98f58cfffd
Amaranth01 <vanessa.nectoux@gmail.com>
2022-11-03 09:06:46 +0100

    add transition

--- public/assets/app.js
+++ public/assets/app.js
@@ -4,13 +4,23 @@ function zoom() {
     for(let i = 0; i < logo.length; i++) {
         logo[i].addEventListener("mouseover", function () {
             logo[i].style.transform = "scale(2.5)";
-            logo[i-1].style.transform = "scale(1.25)";
-            logo[i+1].style.transform = "scale(1.25)";
+            logo[i].style.transition = "0.5s";
+
+            logo[i-1].style.transform = "scale(1.5)";
+            logo[i-1].style.transition = "0.5s";
+
+            logo[i+1].style.transform = "scale(1.5)";
+            logo[i+1].style.transition = "0.5s";
         });
         logo[i].addEventListener("mouseleave", function () {
             logo[i].style.transform = "scale(1)";
+            logo[i].style.transition = "0.5s";
+
             logo[i-1].style.transform = "scale(1)";
+            logo[i-1].style.transition = "0.5s";
+
             logo[i+1].style.transform = "scale(1)";
+            logo[i+1].style.transition = "0.5s";
         });
     }
 }
